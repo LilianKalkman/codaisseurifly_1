c228ca18e4b3528be1c0388f84ee34d13d002246
LilianKalkman <liliankalkman@hotmail.com>
2018-01-19 23:15:14 +0100

    added model association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119211607) do
+ActiveRecord::Schema.define(version: 20180119221427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,13 @@ ActiveRecord::Schema.define(version: 20180119211607) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "song_id"
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+    t.index ["song_id"], name: "index_songs_on_song_id"
   end
 
   add_foreign_key "artists", "songs"
+  add_foreign_key "songs", "artists"
+  add_foreign_key "songs", "songs"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+Artist.destroy
+
+artist_1 = Artist.create!({name: "Britney Spears", image_url:"niks.jpg"})
+artist_2 = Artist.create!({name: "Justin Bieber", image_url:"niks.jpeg"})
+artist_3 = Artist.create!({name: "Spicegirls", image_url:"leeg.jpg"})
+
+song1 = Song.create!({name: "Hit me baby one more time", artist: artist_1})
+song11 = Song.create!({name: "Britney Bitch", artist: artist_1})
+
+song2 = Song.create!({name: "Mama I love you", artist: artist_2})
+song22 = Song.create!({name: "Runaway", artist: artist_2})
+
+song3 = Song.create!({name: "Love Yourself", artist: artist_3})
+song33 = Song.create!({name: "Sorry", artist: artist_3})
