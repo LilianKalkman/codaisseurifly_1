20e93bc0c5922cd6507d78839d9ef6159bfb2f6c
LilianKalkman <liliankalkman@hotmail.com>
2018-01-20 10:15:41 +0100

    Added pictures to artists with cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Song.destroy_all
 Artist.destroy_all
 
-artist_1 = Artist.create!({name: "Britney Spears", image_url:"niks.jpg"})
-artist_2 = Artist.create!({name: "Justin Bieber", image_url:"niks.jpeg"})
-artist_3 = Artist.create!({name: "Spicegirls", image_url:"leeg.jpg"})
+artist_1 = Artist.create!({name: "Britney Spears", image_url:"http://res.cloudinary.com/dknq5n8bi/image/upload/v1516388628/britney_spears-wide_nxhxnv.jpg"})
+artist_2 = Artist.create!({name: "Justin Bieber", image_url:"http://res.cloudinary.com/dknq5n8bi/image/upload/v1516388719/justin-bieber-gq-0316-02_qwcfah.jpg"})
+artist_3 = Artist.create!({name: "Spicegirls", image_url:"http://res.cloudinary.com/dknq5n8bi/image/upload/v1516388665/spicegirls_ha8y32.jpg"})
 
 song1 = Song.create!({name: "Hit me baby one more time", artist: artist_1})
 song11 = Song.create!({name: "Britney Bitch", artist: artist_1})
