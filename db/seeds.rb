ef2fd934ba83f3b8addd9390c377f6844b0f0b17
LilianKalkman <liliankalkman@hotmail.com>
2018-01-20 10:37:22 +0100

    Added showpage with songs of artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ artist_3 = Artist.create!({name: "Spicegirls", image_url:"http://res.cloudinary.
 song1 = Song.create!({name: "Hit me baby one more time", artist: artist_1})
 song11 = Song.create!({name: "Britney Bitch", artist: artist_1})
 
-song2 = Song.create!({name: "Mama I love you", artist: artist_2})
-song22 = Song.create!({name: "Runaway", artist: artist_2})
+song2 = Song.create!({name: "Mama I love you", artist: artist_3})
+song22 = Song.create!({name: "Runaway", artist: artist_3})
 
-song3 = Song.create!({name: "Love Yourself", artist: artist_3})
-song33 = Song.create!({name: "Sorry", artist: artist_3})
+song3 = Song.create!({name: "Love Yourself", artist: artist_2})
+song33 = Song.create!({name: "Sorry", artist: artist_2})
