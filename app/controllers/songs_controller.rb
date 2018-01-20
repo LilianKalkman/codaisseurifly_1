e2a91f007df484fc0c259f1f66409708a91d5b60
LilianKalkman <liliankalkman@hotmail.com>
2018-01-20 18:17:11 +0100

    Added form in new view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,33 +1,34 @@
 class SongsController < ApplicationController
- before_action :set_artist
+ # before_action :set_artist
 
   def index
     @song = @artist.songs
   end
 
-  # def new
-  #   @song = @artist.songs.build
-  # end
-
-  # def create
-  #   @song = @artist.songs.build(song_params)
-  #   @song.artist = Artist.find(params[:artist_id])
-  #
-  #   if @song.save
-  #     redirect_to @song.artist
-  #   else
-  #     render 'new'
-  #   end
-  # end
-  #
-  # def song_params
-  #   params
-  #   .require(:song)
-  #   .permit(:name)
-  # end
-
-  def set_artist
-    @artist = Artist.find(params[:artist_id])
+  def show
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = @artist.songs
+    @song = @song.new(song_params)
+
+    if @song.save
+      redirect_to @artists
+    else
+      render :new_artist_song_path
+    end
+  end
+
+  def song_params
+    params.require(:song).permit(:name)
+  end
+
+  # def set_artist
+  #   @artist = Artist.find(params[:artist_id])
+  # end
 
 end
