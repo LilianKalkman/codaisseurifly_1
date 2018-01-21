dfc2dbdd74c69b13f04ef8055b4f70356bc8a8b6
LilianKalkman <liliankalkman@hotmail.com>
2018-01-21 11:52:08 +0100

    last version

---
+++ spec/views/index_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+#
+# describe "artists/index.html.erb" do
+#   let(:artist) {create :artist}
+#
+#   it "renders all artists" do
+#     render
+#     expect(rendered).to have_content artists.each
+#   end
+# end
