7167b62d839297392cf775b5dad5af1fc8c76d34
LilianKalkman <liliankalkman@hotmail.com>
2018-01-25 19:19:37 +0100

    Added API for artist show

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -6,4 +6,13 @@ class Api::ArtistsController < ApplicationController
     }.to_json
   end
 
+  def show
+  artist = Artist.find(params[:id])
+
+    render status: 200, json: {
+      artist: artist
+    }.to_json
+  end
+
+
 end
