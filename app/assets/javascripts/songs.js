671032574c2033de239dba1ff81af63f4cb1480f
LilianKalkman <liliankalkman@hotmail.com>
2018-01-27 13:17:25 +0100

    Add songs via Ajax

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -4,52 +4,65 @@ function createNewSong(name) {
 
   var newSong = {name: name}
 
-  $.ajax({
-    type: "POST",
-    url: "api/artists"
-    data: JSON.stringify({
-        song: newSong
-    }),
-    contentType: "application/json",
-    dataType: "json"
-  });
-  .done(function(data) {
-    console.log(data);
+    $.ajax({
+      type: "POST",
+      url: "api/artists"
+      data: JSON.stringify({
+          song: newSong
+        }),
+        contentType: "application/json",
+        dataType: "json"
+      });
+        .done(function(data) {
+          console.log(data);
+
+          var checkboxId = "song-" + nextSongId();
+
+          nextSongId() {
+            return $(".song").length + 1;
+          }
+
+          var listItem = $("<li></li>")
+          listItem.addClass("songs");
+
+          var checkbox = $('<input>');
+          checkbox.attr('type', 'checkbox');
+          checkbox.attr('id', checkboxId);
+          // checkbox.val(1)
+
+          var label = $('<label></label>');
+          label.attr('for', checkboxId);
+          label.html(name);
+
+          listItem.append(checkbox);
+          listItem.append(label);
+
+          $(".songs").append(listItem);
+
+}
 
 
-  var checkboxId = "song-" + nextSongId();
+      function submitNewSong(event) {
+        event.preventDefault();
+        var name = $("#new-song").val();
+        createNewSong(name);
+        $("new-song").val(null);
+      }
 
-  nextSongId() {
-    return $(".song").length + 1;
-  }
 
-  var listItem = $("<li></li>")
-  listItem.addClass("songs");
-
-  var checkbox = $('<input>');
-  checkbox.attr('type', 'checkbox');
-  checkbox.attr('id', checkboxId);
-  // checkbox.val(1)
-
-  var label = $('<label></label>');
-  label.attr('for', checkboxId);
-  label.html(name);
-
-  listItem.append(checkbox);
-  listItem.append(label);
-
-  $(".songs").append(listItem);
+function removeSongs(event) {
+  event.preventDefault();
+  $(".songs").remove();
 }
 
 
 
-function submitNewSong(event) {
-    event.preventDefault();
-    var name = $("#new-song").val();
-    createNewSong(name);
-    $("new-song").val(null);
-  }
+
+
+
+
 
 $(document).ready(function() {
   $("form").bind('submit', submitNewSong);
-});
+  $("#remove-songs").bind('click', removeSongs);
+};
